c8c31d3aae2a28af944c535b5f5efb5fd97e3e04
swebb1 <swebb1@staffmail.ed.ac.uk>
2019-03-29 15:48:35 +0000

    multipleGenes

--- app.R
+++ app.R
@@ -18,6 +18,7 @@ a<-readRDS("../data/annotation.rds") ###Sort out factors
 r<-readRDS("../data/rna_data.rds") ### sort out missing data
 
 cols<-c(brewer.pal(n = 9,name = "Set1")[3],"grey39")
+cols2<-c(brewer.pal(n = 9,name = "Set1")[2],"grey39")
 
 ui<-shinyUI(dashboardPage(
   title="Gene explorer",
@@ -27,7 +28,7 @@ ui<-shinyUI(dashboardPage(
                    sidebarMenu(
                      #sidebarSearchForm(textId = "searchText", buttonId = "searchButton",label = "Search..."),
                      menuItem("Home",tabName="home",icon=shiny::icon("home")),
-                     menuItem("Multiple Gene Analysis",tabName="multi",icon=shiny::icon("home")),
+                     menuItem("Multiple Gene Analysis",tabName="multi",icon=shiny::icon("chart-bar")),
                      menuItem("Data Table",tabName="data",icon=shiny::icon("database"))
                    )
   ),
@@ -50,11 +51,7 @@ ui<-shinyUI(dashboardPage(
                   plotOutput("rnaPlot")
                 ),
                 box(
-                  title="Spencer Data",width = 12,status="primary",solidHeader=F,
-                  plotlyOutput("spencerPlot")
-                ),
-                box(
-                  title="Ly/Barr Data",width = 12,status="primary",solidHeader=F
+                  title="Proteomics Data",width = 12,status="primary",solidHeader=F
                   #plotOutput("lyPlot")
                 ),
                 box(
@@ -66,15 +63,9 @@ ui<-shinyUI(dashboardPage(
       tabItem(tabName="multi",
               fluidRow(
                 box(
-                  title = "Multiple Gene Analysis", width = 12, status = "primary",solidHeader=TRUE
-                  #div(style = 'overflow-x: scroll', dataTableOutput('table'))
-                )
-              )
-      ),
-      tabItem(tabName="data",
-              fluidRow(
-                box(
-                  title = "Data Table", width = 12, status = "primary",solidHeader=TRUE
+                  title = "Multiple Gene Analysis", width = 12, status = "primary",solidHeader=F,
+                  textAreaInput("multi",label = "Paste gene list",resize = "both",height = 250),
+                  plotOutput("multiPlot")
                   #div(style = 'overflow-x: scroll', dataTableOutput('table'))
                 )
               )
@@ -90,7 +81,7 @@ server <- function(input, output){
   output$anno = DT::renderDT({
     return(DT::datatable(a,selection = 'single',filter="bottom",
                          options = list(bSortClasses = TRUE,
-                                        aLengthMenu = c(5,10,20,50),
+                                        aLengthMenu = c(1,5,10,20,50),
                                         pageLength = 5
                                         )))
   })
@@ -107,9 +98,10 @@ server <- function(input, output){
   
   output$linksTable = DT::renderDT({
     gene<-genes()
+    ensembl<-paste0("<a href='https://www.ensembl.org/Homo_sapiens/Gene/Idhistory?g=",gene$Gene.ID,"' target='_blank'>Ensembl</a>")
     geneC<-paste0("<a href='https://www.genecards.org/cgi-bin/carddisp.pl?gene=",gene$Gene.ID,"' target='_blank'>Gene Cards</a>")
     hpa<-paste0("<a href='https://www.proteinatlas.org/",gene$Gene.ID,"' target='_blank'>Human Protein Atlas</a>")
-    df<-data.frame(links=c(geneC,hpa))
+    df<-data.frame(links=c(ensembl,geneC,hpa))
     names(df)<-gene$Gene.Symbol
     return(DT::datatable(df,escape = F,options=list(paging=F,searching=F)))
   })
@@ -126,40 +118,34 @@ server <- function(input, output){
       geom_col(aes(x=Experiment,y=log2FoldChange,fill=fill))+
       scale_fill_manual(values = values)+
       theme_bw()+
-      theme(axis.text.x = element_text(angle = 90, hjust = 1,size=10),legend.title = element_blank())+
+      theme(text = element_text(size=20),legend.title = element_blank())+
       ggtitle(as.character(gene$Gene.Symbol))+
       facet_grid(Lab~.,scales="free",space="free")+
       coord_flip()
     g1
   })
   
-  output$gilPlot <- renderPlotly({
-    gene<-genes()
-    gs<-subset(g,g$Gene.ID %in% gene$Gene.ID) %>% spread(measurement,value)
-    gs$adj.p.value<-factor(gs$adj.p.value)
-    gs$adj.p.value<-fct_recode(gs$adj.p.value,`Significant <=0.05`="1",`>0.05`="0")
-    
-    g1<-ggplot(gs,aes(x=Experiment,y=log2FoldChange,fill=adj.p.value,text=paste("padj:",padj,"baseMean:",baseMean)))+
-      geom_col(position = position_dodge(width=1))+
-      theme_bw()+scale_fill_manual(values = c(`Significant <=0.05`=cols[1],`>0.05`=cols[2]))+
-      theme(axis.text.x = element_text(angle = 90, hjust = 1,size=10))+
-      ggtitle(as.character(gene$Gene.Symbol))
-    ggplotly(g1)
+  output$multiPlot <- renderPlot({
+    sig=input$sig
+    genes<-strsplit(input$multi,"\n")[[1]]
+    if(length(genes>=1)){
+      rs<-subset(r,r$Gene.ID %in% genes) %>% spread(measurement,value)
+      
+      g1<-ggplot(rs,aes(x=Experiment,y=log2FoldChange))+
+        geom_boxplot(fill=cols2[1])+geom_jitter(colour=cols2[2])+
+        theme_bw()+
+        theme(text = element_text(size=20),legend.title = element_blank())+
+        ggtitle("Gene Set")+
+        facet_grid(Lab~.,scales="free",space="free")+
+        coord_flip()
+      return(g1)
+    }
+    else{
+      return(NULL)
+    }
   })
-
-  output$spencerPlot <- renderPlotly({
-    gene<-genes()
-    ss<-subset(s,s$Gene.ID %in% gene$Gene.ID) %>% spread(measurement,value)
-    ss$adj.p.value<-factor(ss$adj.p.value)
-    ss$adj.p.value<-fct_recode(ss$adj.p.value,`Significant <=0.05`="1",`>0.05`="0")
-    
-    g2<-ggplot(ss,aes(x=Experiment,y=log2FoldChange,fill=adj.p.value,text=paste("padj:",padj)))+
-      geom_col(position = position_dodge2(width=1,preserve = "single"))+
-      theme_bw()+scale_fill_manual(values = c(`Significant <=0.05`=cols[1],`>0.05`=cols[2]))+
-      theme(axis.text.x = element_text(angle = 90, hjust = 1,size=10))+
-      ggtitle(as.character(gene$Gene.Symbol))
-    ggplotly(g2)
-  })
+  
+  
 }
 
 shinyApp(ui = ui, server = server)
